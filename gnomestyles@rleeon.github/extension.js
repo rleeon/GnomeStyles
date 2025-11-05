ecac015a8b4b48f90edcd3dddb206e0f78c28d3e
Mr. Insider <raileonoliva@gmail.com>
2025-11-05 12:37:22 +0100

    Add files via upload

--- gnomestyles@rleeon.github/extension.js
+++ gnomestyles@rleeon.github/extension.js
@@ -1,19 +1,7 @@
 /* extension.js
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * Codigo en español porque es mi lenguaje nativo.
+ * 
  */
 
 import GObject from 'gi://GObject';
@@ -35,7 +23,7 @@ class Indicator extends PanelMenu.Button {
             style_class: 'system-status-icon',
         }));
 
-        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
+        let item = new PopupMenu.PopupMenuItem(_('Show ENEMYS'));
         item.connect('activate', () => {
             Main.notify(_('Whatʼs up, folks?'));
         });
